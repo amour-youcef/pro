ad36d75a8f6fbb1f75cafd1e71fecd56ca4771a5
dev <amour-youcef@hotmail.com>
2020-04-30 20:34:29 +0200

    30042020

--- Spring MVC CRUD/src/main/java/com/spring/crud/Databaseinfo.java
+++ Spring MVC CRUD/src/main/java/com/spring/crud/Databaseinfo.java
@@ -1,5 +1,13 @@
 package com.spring.crud;
 
+import org.springframework.jdbc.core.JdbcTemplate; 
+
 public class Databaseinfo {
 
+	private JdbcTemplate jdbcTemplate;
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}  
+	
 }
