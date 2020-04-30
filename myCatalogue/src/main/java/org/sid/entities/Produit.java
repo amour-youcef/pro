463dd7c51a6ccf8ecdc8927b8b8e78cb0013fa84
dev <amour-youcef@hotmail.com>
2020-05-01 01:55:22 +0200

    01052020

---
+++ myCatalogue/src/main/java/org/sid/entities/Produit.java
@@ -0,0 +1,26 @@
+package org.sid.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor @ToString
+public class Produit {
+	
+		@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String designation;
+	private double prix;
+	private double quantite;
+	
+	
+	
+}
